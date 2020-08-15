e4239b70b9a22d8cc867483ead7f2c5448bd1460
GGG <ggg@MacBook-Pro-de-GGG.local>
2020-08-15 19:11:20 +0200

    T add websocket

--- index.js
+++ index.js
@@ -12,6 +12,21 @@ const userRoutes = require("./routes/user");
 app.use(userRoutes);
 const parking2RRoutes = require("./routes/parking2R");
 app.use(parking2RRoutes);
+//
+const WebSocket = require("ws");
+const wss = new WebSocket.Server({ port: process.env.PORT || 8080 }, () => {
+  console.log("Server started");
+});
+wss.on("connection", (connection) => {
+  connection.on("message", (message) => {
+    // Transmettre un message à tous les autres utilisateurs connectés (broadcast)
+    wss.clients.forEach((client) => {
+      if (client !== connection && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+});
 //BDD
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
